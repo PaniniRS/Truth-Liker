808a2878a2676b00a32913aa6dcc87364eea7b6a
PaniniRS <bubemanchester@live.com>
2023-10-30 14:54:27 +0100

    seperated table printing code for readability

--- dbmanager.py
+++ dbmanager.py
@@ -3,8 +3,43 @@ import time
 import glob
 
 #TODO: Make first value a key
+#TODO: REFACTOR CODE
 
-run = True
+
+def printTable():
+        #clear console
+    print("\033[H\033[J")
+    #print the database and table connected to
+    print("--------------------------------------------------")
+    print(" 📌 DB Connected: " + userChoiceDB + ".db         ")
+    print(" 📌 Table Connected: " + userChoiceTable + "         ")
+    #print all the tables in the database with their entries
+    c = conn.cursor()
+    c.execute("""SELECT name FROM sqlite_master WHERE type='table' ORDER BY name;""")
+    tables = c.fetchall()
+    for table in tables:
+        c.execute(f"""SELECT COUNT(*) FROM {table[0]}""")
+        entries = c.fetchall()
+        print(f" \t ⭕ {table[0]} - {entries[0][0]} entries")
+    
+    print("--------------------------------------------------")
+    print("0. Connect to a different database                ")
+    print("1. Connect to a table                             ")
+    print("2. Create a new database                          ")
+    print("3. Add a new table                                ")
+    print("4. Add a new entry(row))                          ")
+    print("                                                  ")
+    print("5. View all entries(rows)                         ")
+    print("                                                  ")
+    print("6. Delete a entry(row)                            ")
+    print("7. Delete a table                                 ")
+    print("8. Delete a database                              ")
+    print("                                                  ")
+    print("9. Exit                                           ")
+    print("--------------------------------------------------")
+
+
+run = True #whether to run the main loop or not
 print("--------------------------------------------------")
 print("           Welcome to the DB Manager              ")
 print("--------------------------------------------------")
@@ -22,41 +57,28 @@ time.sleep(0.2)
 print("Connected to database successfully")
 time.sleep(0.5)
 
+#ask user to select a table to connect to 
+c = conn.cursor()
+#print all the tables available in the database
+c.execute("""SELECT name FROM sqlite_master WHERE type='table' ORDER BY name;""")
+tables = c.fetchall()
+print("📁 | TABLES FOUND:")
+for table in tables:
+    print(table[0])
+print("--------------------------------------------------")
+userChoiceTable = input("Enter the table you want to connect to: ")
+
+
 #main loop
 while run:
-    print("--------------------------------------------------")
-    print(" 📌 DB Connected: " + userChoiceDB + ".db         ")
-    #display tables in connected database and the number of entries they have
-    c = conn.cursor()
-    c.execute("""SELECT name FROM sqlite_master WHERE type='table' ORDER BY name;""")
-    tables = c.fetchall()
-    for table in tables:
-        c.execute(f"""SELECT COUNT(*) FROM {table[0]}""")
-        entries = c.fetchall()
-        print(f" ⭕ {table[0]} - {entries[0][0]} entries")
-    print("                                                  ")
-    print("--------------------------------------------------")
-    print("0. Connect to a different database                ")
-    print("1. Create a new database                          ")
-    print("2. Add a new table                                ")
-    print("3. Add a new entry(row))                          ")
-    print("                                                  ")
-    print("4. View all entries(rows)                         ")
-    print("                                                  ")
-    print("5. Delete a entry(row)                            ")
-    print("6. Delete a table                                 ")
-    print("7. Delete a database                              ")
-    print("                                                  ")
-    print("8. Exit                                           ")
-    print("--------------------------------------------------")
-
+    printTable()
     choice = input("Enter your choice: ")
 
-    while choice not in ["0","1", "2", "3", "4", "5", "6", "7", "8"]:
+    while choice not in ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"]:
         print("Invalid input")
         choice = input("Enter your choice: ")
+    #connect to a different database
     if choice == "0":
-        #connect to a different database
         conn.close()
         userChoiceDB = input("Enter database you want to connect to: ")
         conn = sqlite3.connect(userChoiceDB + ".db")
@@ -64,8 +86,15 @@ while run:
         time.sleep(0.2)
         print("Connected to database successfully")
         time.sleep(0.5)
+    #connect to a different table
     elif choice == "1":
-        #create a new database
+        c = conn.cursor()
+        userChoiceTable = input("Enter the table you want to connect to: ")
+        c.execute(f"""SELECT COUNT(*) FROM {userChoiceTable}""")
+        entries = c.fetchall()
+        print(f"Connected to table {userChoiceTable} successfully")
+    #create a new database
+    elif choice == "2":
         conn.close()
         userChoiceDB = input("Enter a name for your database: ")
         conn = sqlite3.connect(userChoiceDB + ".db")
@@ -74,32 +103,29 @@ while run:
         conn.commit()
         print("Database created successfully")
         time.sleep(0.2)
-    elif choice == "2":
-        #add a new table
+    #add a new table
+    elif choice == "3":
         c = conn.cursor()
-        tableName = input("Enter a name for your table: ") #TODO: make it so you only selec it once so you dont have to ask every time and repeat code
-        c.execute(f"""CREATE TABLE IF NOT EXISTS {tableName} (user, pw)""")
+        c.execute(f"""CREATE TABLE IF NOT EXISTS {userChoiceTable} (user, pw)""")
         conn.commit()
         print("Table created successfully")
         time.sleep(0.2)
-    elif choice == "3":
-        #add a new row
+    #add a new row
+    elif choice == "4":
         c = conn.cursor()
-        tableName = input("Enter the name of the table: ")
         user = input("Enter the username: ")
         pw = input("Enter the password: ")
-        c.execute(f"""INSERT INTO {tableName} VALUES (?, ?)""", (user, pw))
+        c.execute(f"""INSERT INTO {userChoiceTable} VALUES (?, ?)""", (user, pw))
         conn.commit()
         print("Row added successfully")
         time.sleep(0.2)
-    elif choice == "4":
-        #view all rows
+    #view all rows
+    elif choice == "5":
         conn = sqlite3.connect(userChoiceDB + ".db")
         c = conn.cursor()
-        tableName = input("Enter the name of the table: ")
         #fail safe if table doesnt exits
         try:
-            c.execute(f"""SELECT rowid, * FROM {tableName}""")
+            c.execute(f"""SELECT rowid, * FROM {userChoiceTable}""")
             for row in c.fetchall():
                 print(row)
         except:
@@ -109,27 +135,25 @@ while run:
         conn.commit()
         print("Entries(rows) displayed successfully")
         time.sleep(0.2)
-    elif choice == "5":
-        #delete a row
+    #delete a row
+    elif choice == "6":
         conn = sqlite3.connect(userChoiceDB + ".db")
         c = conn.cursor()
-        tableName = input("Enter the name of the table: ")
         rowId = input("Enter the row id: ")
-        c.execute(f"""DELETE FROM {tableName} WHERE rowid = {rowId}""") #ERROR: doesnt work for some reason, the row id doesnt exits, also put it in a try except so it doesnt crash
+        c.execute(f"""DELETE FROM {userChoiceTable} WHERE rowid = {rowId}""") #ERROR: doesnt work for some reason, the row id doesnt exits, also put it in a try except so it doesnt crash
         conn.commit()
         print("Row deleted successfully")
         time.sleep(0.2)
-    elif choice == "6":
-        #delete a table
+    #delete a table
+    elif choice == "7":
         conn = sqlite3.connect(userChoiceDB + ".db")
         c = conn.cursor()
-        tableName = input("Enter the name of the table: ")
-        c.execute(f"""DROP TABLE {tableName}""")
+        c.execute(f"""DROP TABLE {userChoiceTable}""")
         conn.commit()
         print("Table deleted successfully")
         time.sleep(0.2)
-    elif choice == "7":
-        #delete a database
+    #delete a database
+    elif choice == "8":
         userChoiceDB = input("Enter the name of the database: ")
         conn = sqlite3.connect(userChoiceDB + ".db")
         c = conn.cursor()
@@ -138,8 +162,8 @@ while run:
         conn.close()
         print("Database deleted successfully")  
         time.sleep(0.2)
-    elif choice == "8":
-        #exit
+    #exit
+    elif choice == "9":
         conn.close()
         print(f"Exiting...")
         time.sleep(0.2)
